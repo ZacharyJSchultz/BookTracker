6903fdcb1a2bf795c7b53267f68e7422067fb388
ZachAttack1170 <Zachjacob7@gmail.com>
2024-08-07 23:03:46 -0400

    Testing connectivity btwn server and client; successful

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import NavBar from './NavBar'
 import { BrowserRouter } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [bookData, setBookData] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setBookData(data.message));
+  }, []);
+
   return (
-    <BrowserRouter>
-      <NavBar />
-    </BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <NavBar />
+      </BrowserRouter>
+      <h1>{bookData}</h1>
+    </div>
   );
 }
 
